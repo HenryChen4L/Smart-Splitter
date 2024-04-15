46beb3633989492eed2a2d8cad2a8009fc475d9c
Zhengyu Chen <chen.zhengy@northeastern.edu>
2024-04-14 20:11:48 -0400

    feat: add transaction class definition

--- src/transaction/Transaction.java
+++ src/transaction/Transaction.java
@@ -1,5 +1,54 @@
 package transaction;
 
-public class Transaction {
+import java.util.HashMap;
 
+import splitter.Splitter;
+import splitter.SplitterDirectory;
+
+public class Transaction {
+	
+	private double total;
+	private String transactionName;
+	private String date;
+	private SplitterDirectory splitterDirectory;
+	private HashMap<Splitter, Double> transactionDetails;
+	public Transaction(double total, String transactionName, String date, SplitterDirectory splitterDirectory, HashMap<Splitter, Double> transactionDetails) {
+		super();
+		this.total = total;
+		this.transactionName = transactionName;
+		this.date = date;
+		this.splitterDirectory = splitterDirectory;
+		this.transactionDetails = transactionDetails;
+	}
+	public double getTotal() {
+		return total;
+	}
+	public void setTotal(double total) {
+		this.total = total;
+	}
+	public String getTransactionName() {
+		return transactionName;
+	}
+	public void setTransactionName(String transactionName) {
+		this.transactionName = transactionName;
+	}
+	public String getDate() {
+		return date;
+	}
+	public void setDate(String date) {
+		this.date = date;
+	}
+	public SplitterDirectory getSplitterDirectory() {
+		return splitterDirectory;
+	}
+	public void setSplitterDirectory(SplitterDirectory splitterDirectory) {
+		this.splitterDirectory = splitterDirectory;
+	}
+	public HashMap<Splitter, Double> getTransactionDetails() {
+		return transactionDetails;
+	}
+	public void setTransactionDetails(HashMap<Splitter, Double> transactionDetails) {
+		this.transactionDetails = transactionDetails;
+	}
+	
 }
--- src/transaction/TransactionDirectory.java
+++ src/transaction/TransactionDirectory.java
@@ -1,6 +1,10 @@
 package transaction;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+
+import splitter.Splitter;
+import splitter.SplitterDirectory;
 
 public class TransactionDirectory {
 
@@ -10,8 +14,8 @@ public class TransactionDirectory {
 		this.transactionDirectory = new ArrayList<Transaction>();
 	}
 	
-	public Transaction addNewTransaction() {
-		Transaction transaction = new Transaction();
+	public Transaction addNewTransaction(double total, String transactionName, String date, SplitterDirectory splitterDirectory, HashMap<Splitter, Double> transactionDetails) {
+		Transaction transaction = new Transaction(total, transactionName, date, splitterDirectory, transactionDetails);
 		this.transactionDirectory.add(transaction);
 		return transaction;
 	}
